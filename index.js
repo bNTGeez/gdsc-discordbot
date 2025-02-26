bcc992ea84466c6e84919a275e1bbb3331d07bff
bNTGeez <tangbenjamin123@gmail.com>
2025-02-25 19:54:29 -0800

    Your commit message here

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits, Collection } = require("discord.js");
+const {
+  Client,
+  Events,
+  GatewayIntentBits,
+  Collection,
+  Partials,
+} = require("discord.js");
 const fs = require("node:fs");
 const path = require("node:path");
 
@@ -10,8 +16,9 @@ const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessageReactions,
   ],
+  partials: [Partials.Message, Partials.Channel, Partials.Reaction],
 });
 
 // When the client is ready, run this code (only once).
@@ -37,7 +44,7 @@ for (const folder of commandFolders) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
 
-    console.log(command.data.description)
+    console.log(command.data.description);
     // Set a new item in the Collection with the key as the command name and the value as the exported module
     if ("data" in command && "execute" in command) {
       client.commands.set(command.data.name, command);
@@ -71,5 +78,21 @@ client.on(Events.InteractionCreate, async (interaction) => {
     }
   }
 });
+
+// Add this section for event handling
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
 
 client.login(process.env.DISCORDTOKEN);
